b3f8ca9e2e22dcf6d2ed3f567263d791a33db272
Nina Chyzhevska <nina_chyzhevska@epam.com>
2023-10-15 23:05:03 +0300

    Add integration with logging framework

--- src/test/java/com/nina/testsuite/example/GoogleTest.java
+++ src/test/java/com/nina/testsuite/example/GoogleTest.java
@@ -4,6 +4,8 @@ import com.nina.pages.example.GooglePage;
 import com.nina.pages.example.SearchResultsPage;
 import com.nina.testsuite.BaseTest;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
@@ -11,8 +13,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleTest extends BaseTest {
 
+    private final Logger logger = LoggerFactory.getLogger(GoogleTest.class);
+
     @Test
     public void userCanSearch() {
+        logger.info("This is a test method to verify that Selenide integration works.");
+        logger.warn("This test will be removed soon");
+
         open("https://duckduckgo.com");
         new GooglePage().searchFor("selenide java");
 
