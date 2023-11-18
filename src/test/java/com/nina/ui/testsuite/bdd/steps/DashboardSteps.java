37d04217bdecd875c2672084dcd4d0d2599cb546
Nina <ninachyzhevska@gmail.com>
2023-11-19 00:47:17 +0200

    Add hooks

--- src/test/java/com/nina/ui/testsuite/bdd/steps/DashboardSteps.java
+++ src/test/java/com/nina/ui/testsuite/bdd/steps/DashboardSteps.java
@@ -2,18 +2,55 @@ package com.nina.ui.testsuite.bdd.steps;
 
 import com.nina.ui.steps.DashboardsSteps;
 import com.nina.ui.steps.LoginSteps;
+import com.nina.ui.util.driver.DriverManager;
 import com.nina.util.EnvironmentPropertyLoader;
+import io.cucumber.java.*;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class DashboardSteps {
 
+    private static final Logger logger = LoggerFactory.getLogger(DashboardSteps.class);
     private static final String TEST_DASHBOARD_NAME = "demo";
     private final DashboardsSteps dashboardsSteps = new DashboardsSteps();
+
+    @BeforeAll
+    public static void init() {
+        logger.info("Driver initialization");
+        DriverManager.initDriver();
+    }
+
+    @AfterAll
+    public static void shutDown() {
+        logger.info("Browser shut down");
+        DriverManager.shutDownDriver();
+    }
+
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        logger.info("Starting scenario execution: " + scenario.getName());
+    }
+
+    @After
+    public void afterScenario(Scenario scenario) {
+        logger.info("Finishing scenario execution: " + scenario.getName());
+    }
+
+    @BeforeStep
+    public void beforeStep() {
+        logger.info("[Example] This block of code is executed before step");
+    }
+
+    @AfterStep
+    public void afterStep() {
+        logger.info("[Example] This block of code is executed after step");
+    }
 
     @When("I search the dashboard by name")
     public void searchTheDashboardByName() {
