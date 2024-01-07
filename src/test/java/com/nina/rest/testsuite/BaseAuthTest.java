a9fc9cd653ad77e219ae3fc267a7ba409b6a6bc2
Nina <ninachyzhevska@gmail.com>
2024-01-07 09:43:35 +0200

    Submit REST tests to report portal

--- src/test/java/com/nina/rest/testsuite/BaseAuthTest.java
+++ src/test/java/com/nina/rest/testsuite/BaseAuthTest.java
@@ -1,12 +1,15 @@
 package com.nina.rest.testsuite;
 
+import com.epam.reportportal.junit5.ReportPortalExtension;
 import com.nina.rest.client.AuthClient;
 import com.nina.rest.client.http.HttpClient;
 import com.nina.rest.client.http.HttpClientFactory;
 import com.nina.rest.model.UserSession;
 import com.nina.util.EnvironmentPropertyLoader;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(ReportPortalExtension.class)
 public class BaseAuthTest {
     protected static HttpClient httpClient;
     protected static UserSession userSession;
