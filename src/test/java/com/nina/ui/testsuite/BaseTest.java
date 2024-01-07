9aa37bf7afffbf21f15d6d752cbc677841928992
Nina <ninachyzhevska@gmail.com>
2024-01-07 09:40:41 +0200

    Add integration with report portal

--- src/test/java/com/nina/ui/testsuite/BaseTest.java
+++ src/test/java/com/nina/ui/testsuite/BaseTest.java
@@ -1,9 +1,12 @@
 package com.nina.ui.testsuite;
 
+import com.epam.reportportal.junit5.ReportPortalExtension;
 import com.nina.ui.util.driver.DriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(ReportPortalExtension.class)
 public abstract class BaseTest {
     @BeforeAll
     static void init() {
